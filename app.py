835c9054992814ee432bf1f08339558f0f44e667
sohamds1 <88491377+sohamds1@users.noreply.github.com>
2021-10-24 01:26:36 +0530

    Updating app.py
    Updating the app.py

--- app.py
+++ app.py
@@ -18,5 +18,3 @@ def predict():
     output = round(prediction[0],2)
     return render_template('index.html',prediction_text="Number of Weekly Rides Should be: {}".format(math.floor(output)))
 
-if __name__ == '__main__':
-    app.run()
